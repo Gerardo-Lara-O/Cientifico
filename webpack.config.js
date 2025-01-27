21936b9f06d5d0d9ad8d79a9d864736da3abd8d9
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2025-01-26 22:19:16 -0600

    minificacion del proyecto y agregamos hashes

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 
 module.exports = { // objeto que define la configuracion
@@ -9,7 +11,8 @@ module.exports = { // objeto que define la configuracion
     // mode: "production",
     output: {  // Configuracion de salida
         path: path.resolve(__dirname, 'dist'), // Ruta absoluta para los archivos generados
-        filename: 'main.js', // Nombre del archivo de salida
+        filename: '[name].[contenthash].js', // Nombre del archivo de salida
+        clean: true,
     },
     resolve: { // Configura cómo Webpack resuelve las importaciones en tu proyecto.
         extensions: ['.js'], // Especifica las extensiones de archivo que Webpack debe resolver automáticamente
@@ -41,11 +44,15 @@ module.exports = { // objeto que define la configuracion
             patterns: [
                 {
                     from: path.resolve(__dirname, 'src/styles/styles.css'), // Carpeta de origen
-                    to: 'styles/', // Carpeta de destino dentro de dist
+                    to: 'styles/[name].[contenthash].css', // Carpeta de destino dentro de dist
                 },
             ],
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin(
+            {
+                filename: 'styles/[name].[contenthash].css', // Genera un archivo CSS con hash
+            }
+        ),
 
     ],
     devServer: {
@@ -56,5 +63,18 @@ module.exports = { // objeto que define la configuracion
         historyApiFallback: true, // Soporte para SPAs
         port: 3006, // Puerto del servidor
         open: true, // Abre el navegador automáticamente al iniciar el servidor
-    }
+    },
+    optimization: {
+        minimize: true, // Activa la minimización
+        minimizer: [
+            new CssMinimizerPlugin(), // Minimiza el CSS
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: true, // Elimina console.log del código final
+                    },
+                },
+            }),
+        ],
+    },
 }
